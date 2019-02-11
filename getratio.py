94e897484d5f78414f8ce963d5962b527f2ad97a
Mart Gil Robles <46025304+martgil@users.noreply.github.com>
2019-02-11 10:30:49 +0800

    Add files via upload

---
+++ getratio.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# Author: Mart Gil
+# Date: February 11. 2019
+# Description: simple script to extract aspect ratio of an image
+
+import math
+
+width = input('Enter Image width: ')
+height = input('Enter image height: ')
+
+def gcd(a,b):
+    if(b==0):
+        return a
+    else:
+        return gcd(b,a%b)
+
+d = gcd(width,height)
+
+aspect_width = width / d
+aspect_height = height / d
+
+print 'Your image aspect ratio is ' + str(aspect_width) + ':' + str(aspect_height)
+print 'Crop your image at http://croppola.com/'
+# credits to croppola
+
